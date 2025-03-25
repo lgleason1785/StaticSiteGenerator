43ca427320137f2d7597316f923cf28b99c3a1f3
Logan <logangleason@gmail.com>
2025-03-25 13:47:08 -0700

    Changed logic in split_nodes_delimiter to account for edge cases regarding nodes without delimiters, unmatched delimiters, double delimiters, and empty string inside delimiters. Also added tests to the respective test suite for these corner cases.

--- src/test_split_nodes_delimiter.py
+++ src/test_split_nodes_delimiter.py
@@ -22,4 +22,33 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         ]
         self.assertEqual(split_nodes_delimiter([node], "**", TextType.BOLD), expected_result)
 
+    def test_unmatched_delimiters(self):
+        node = TextNode("This is an **unmatched delimiter", TextType.TEXT)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "**", TextType.BOLD)
     
+    def test_no_delimiters_present(self):
+        node = TextNode("This is plain text", TextType.TEXT)
+        expected_result = [TextNode("This is plain text", TextType.TEXT)]
+        self.assertEqual(split_nodes_delimiter([node], "**", TextType.BOLD), expected_result)
+
+    def test_multiple_delimiters(self):
+        node = TextNode("This is **bold** and another **bold** here", TextType.TEXT)
+        expected_result = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" here", TextType.TEXT)
+        ]
+        self.assertEqual(split_nodes_delimiter([node], "**", TextType.BOLD), expected_result)
+
+    def test_empty_strings_between_delimiter(self):
+        node = TextNode("This has **bold** and an empty **** delimiter", TextType.TEXT)
+        expected_result = [
+            TextNode("This has ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" and an empty ", TextType.TEXT),
+            TextNode(" delimiter", TextType.TEXT)
+        ]
+        self.assertEqual(split_nodes_delimiter([node], "**", TextType.BOLD), expected_result)
